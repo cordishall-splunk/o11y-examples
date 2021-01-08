073efbfe1aa288ec1f8f4d2a007fe8e77ed238d9
Cordis Hall <chall@splunk.com>
2021-01-08 16:14:46 -0700

    readme

---
+++ ecs-fargate-aspentcore/update.sh
@@ -0,0 +1,9 @@
+rm mymvcweb/Dockerfile
+wget https://raw.githubusercontent.com/cordishall-splunk/o11y-examples/ecs-fargate-netcore/ecs-fargate-aspentcore/Samples/mymvcwebSamples/Dockerfile
+rm reverseproxy/Dockerfile
+wget https://raw.githubusercontent.com/cordishall-splunk/o11y-examples/ecs-fargate-netcore/ecs-fargate-aspentcore/Samples/reverseproxySamples/Dockerfile
+sudo docker-compose build
+sudo docker tag amazon-ecs-fargate-aspnetcore_mymvcweb:latest $AWS_ACCOUNT_NUMBER.dkr.ecr.us-west-2.amazonaws.com/mymvcweb:latest 
+sudo docker push $AWS_ACCOUNT_NUMBER.dkr.ecr.us-west-2.amazonaws.com/mymvcweb:latest 
+sudo docker tag amazon-ecs-fargate-aspnetcore_reverseproxy:latest $AWS_ACCOUNT_NUMBER.dkr.ecr.us-west-2.amazonaws.com/reverseproxy:latest
+sudo docker push $AWS_ACCOUNT_NUMBER.dkr.ecr.us-west-2.amazonaws.com/reverseproxy:latest
